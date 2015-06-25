1e8ceffd69a989f43247364a345d984c8042b453
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-06-26 00:16:52 +0545

    deleted commented lines, enabled tagging in annotation

--- public/js/contractmvc.js
+++ public/js/contractmvc.js
@@ -277,6 +277,35 @@ var AnnotatorjsView = Backbone.View.extend({
                 }
             }); 
         }
+        function split( val ) {
+            return val.split( / \s*/ );
+        }
+        function extractLast( term ) {
+            return split( term ).pop();
+        }
+        var availableTags = this.availableTags;
+        this.content.data('annotator').plugins.Tags.input.autocomplete({
+            source: function( request, response) {
+                // delegate back to autocomplete, but extract the last term
+                response( $.ui.autocomplete.filter(
+                        availableTags, extractLast( request.term ) ) );
+            },
+            focus: function() {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function( event, ui ) {
+                var terms = split( this.value );
+                // remove the current input
+                terms.pop();
+                // add the selected item
+                terms.push( ui.item.value );
+                // add placeholder to get the comma-and-space at the end
+                terms.push( "" );
+                this.value = terms.join( " " );
+                return false;
+            }
+        });        
         return this;
     }                   
 });
