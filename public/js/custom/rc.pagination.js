d69d483f41445a65687b2222b40248302a09ec26
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2015-08-04 10:19:00 +0545

    issue-159 Improve page navigation  *goto page design  *goto page contract pages

--- public/js/custom/rc.pagination.js
+++ public/js/custom/rc.pagination.js
@@ -1,7 +1,9 @@
 var PaginationView = Backbone.View.extend({
     initialize: function(options) {
         this.options = options;
+
         this.eventsPipe = options.eventsPipe;
+        this.gotoPageView = options.gotoPageView;
         this.eventsPipe.on("page-change", this.pageChange, this);
         var self = this;
         this.pagination = $(this.el).pagination({
@@ -9,6 +11,7 @@ var PaginationView = Backbone.View.extend({
             displayedPages: 5,
             cssStyle: 'light-theme',
             onPageClick: function(pageNumber, event) {
+                self.gotoPageView.setPageNumber(pageNumber);
                 self.eventsPipe.trigger("page-load", pageNumber);
                 self.eventsPipe.trigger("page-scroll", pageNumber);
             }
@@ -16,8 +19,33 @@ var PaginationView = Backbone.View.extend({
         return this;
     },
     pageChange: function(pageNumber) {
+        this.gotoPageView.setPageNumber(pageNumber);
         this.eventsPipe.trigger("page-load", pageNumber);
         this.pagination.pagination('drawPage', pageNumber);
         this.eventsPipe.trigger("page-scroll", pageNumber);
+    }
+});
+
+var GotoPageView = Backbone.View.extend({
+    events: {
+        "click button": "gotoPage"
+    },
+    initialize: function(options) {
+        this.eventsPipe = options.eventsPipe;
+        this.totalPages = options.totalPages;
+    },
+    setPageNumber:function(page) {
+        $("#goto_page").val(page);
+    },
+    gotoPage: function(e) {
+        page = $("#goto_page").val();
+        e.preventDefault();
+        if($.isNumeric(page) && parseInt(this.totalPages) > parseInt(page)){
+            $(".ql-editor").html("loading!")
+            this.eventsPipe.trigger("page-change", parseInt(page));
+        }else{
+            $("#message").html('<div class="alert alert-danger">Enter valid page number.</div>');
+        }
+
     }
 });
