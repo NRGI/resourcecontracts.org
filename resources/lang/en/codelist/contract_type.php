be9e84af96b01d0f2242f49c32dca5cb7b4dfff3
deepak <deepak.shah@yipl.com.np>
2015-09-11 14:12:41 +0545

    resource and contract type updated

--- resources/lang/en/codelist/contract_type.php
+++ resources/lang/en/codelist/contract_type.php
@@ -1,13 +1,12 @@
 <?php
 return [
-    "Production or Profit Sharing Agreement" => "Production or Profit Sharing Agreement",
-    "Investor - State Agreement"             => "Investor - State Agreement",
-    "Exploration Permit/License"             => "Exploration Permit/License",
-    "Service Contract"                       => "Service Contract",
     "Exploitation Permit/License"            => "Exploitation Permit/License",
-    "Joint Venture Agreement"                => "Joint Venture Agreement",
-    "Permis de recherche"                    => "Permis de recherche",
+    "Exploration Permit/License"             => "Exploration Permit/License",
     "Investment Promotion Agreements"        => "Investment Promotion Agreements",
+    "Investor - State Agreement"             => "Investor - State Agreement",
+    "Joint Venture Agreement"                => "Joint Venture Agreement",
+    "Production or Profit Sharing Agreement" => "Production or Profit Sharing Agreement",
+    "Service Contract"                       => "Service Contract",
     "Stabilization Agreements"               => "Stabilization Agreements",
     "Other"                                  => "Other",
 ];
