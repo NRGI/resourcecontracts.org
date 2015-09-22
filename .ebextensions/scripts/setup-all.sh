856f976c551a3be7d0bec9965a7128abf2d34e1c
Anjesh Tuladhar <anjeshtuladhar@gmail.com>
2015-09-22 08:56:32 +0545

    minor fix, now fully deployable

--- .ebextensions/scripts/setup-all.sh
+++ .ebextensions/scripts/setup-all.sh
@@ -1,3 +1,4 @@
+export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/sbin
 bash /home/ec2-user/scripts/setup-beanstalk.sh
 bash /home/ec2-user/scripts/setup-supervisor.sh
 bash /home/ec2-user/scripts/setup-pdfprocessor.sh
--- .ebextensions/scripts/setup-supervisor.sh
+++ .ebextensions/scripts/setup-supervisor.sh
@@ -4,4 +4,4 @@ cp /home/ec2-user/scripts/conf/supervisord.conf /etc/supervisord.conf
 cp /home/ec2-user/scripts/conf/supervisord.sh /etc/init.d/supervisord
 ln -s /usr/local/bin/supervisord /usr/sbin/supervisord
 chmod 777 /etc/init.d/supervisord
-/etc/init.d/supervisord start
+/etc/init.d/supervisord restart
