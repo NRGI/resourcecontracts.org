1607ca9f757d206ba1d47df0fe93dc0cadeb8d18
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2015-09-28 11:36:49 +0545

    annotation category error mapped and udpated

--- app/Console/Commands/UpdateAnnotation.php
+++ app/Console/Commands/UpdateAnnotation.php
@@ -52,12 +52,28 @@ class UpdateAnnotation extends Command
      */
     public function fire()
     {
-        $contracts = $this->contract->all();
+        $annotation_mapping_category = [
+            'i-general-information',
+            '1-fundamental-provisions',
+            '2-community-and-social-obligations',
+            '3-developers-financial-obligations',
+            '4-environmental-provisions',
+            '5-operational-provisions',
+            '6-miscellaneous-provisions',
+            'iii-document-notes',
+            'ii-summary-of-terms',
+            'environmental-monitoring'
+        ];
+        $contracts                   = $this->contract->all();
         foreach ($contracts as $contract) {
             foreach ($contract->annotations as $annotation) {
                 $this->info("contractID => {$contract->id}");
-                $annotationArray            = json_encode($annotation->annotation);
-                $annotationArray            = json_decode($annotationArray, true);
+                $annotationArray = json_encode($annotation->annotation);
+                $annotationArray = json_decode($annotationArray, true);
+                $category_mapped = array_search($annotationArray['category'], $annotation_mapping_category);
+                if (false !== $category_mapped) {
+                    $annotationArray['category'] = 'other';
+                }
                 $annotationArray['cluster'] = _l(config("annotation_category.cluster.{$annotationArray['category']}"));
                 $annotation->annotation     = $annotationArray;
                 $this->info($annotation->save());
