3f2395f93c2ff59c5008de7a27c04b8015f61779
Manoj Byanjankar <manoj.byanjankar@yipl.com.np>
2015-09-30 14:41:13 +0545

    contract type mapping

--- resources/lang/en/codelist/contract_type.php
+++ resources/lang/en/codelist/contract_type.php
@@ -8,7 +8,20 @@ return [
     "Production or Profit Sharing Agreement" => "Production or Profit Sharing Agreement",
     "Service Contract"                       => "Service Contract",
     "Stabilization Agreements"               => "Stabilization Agreements",
-    "Other"                                  => "Other",
+    "Agricultural Development Agreement"     => "Agricultural Development Agreement",
+    "Bail Foncier"                           => "Bail Foncier",
+    "Contrat de Concession Agricole"         => "Contrat de Concession Agricole",
+    "Establishment Convention"               => "Establishment Convention",
+    "Forest Management Contract"             => "Forest Management Contract",
+    "Investment Incentive Contract"          => "Investment Incentive Contract",
+    "Protocole d'Accord"                     => "Protocole d'Accord",
+    "Concession Agreement"                   => "Concession Agreement",
+    "Timber Sale Contract"                   => "Timber Sale Contract",
+    "Contrat de Concession Forestière"       => "Contrat de Concession Forestière",
+    "Land Lease Agreement"                   => "Land Lease Agreement",
+    "Memorandum of Understanding"            => "Memorandum of Understanding",
+    "Sub-lease"                              => "Sub-lease",
+    "Other"                                  => "Other//Autre",
 ];
 
 
