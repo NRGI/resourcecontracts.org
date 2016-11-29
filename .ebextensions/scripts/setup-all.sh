7ddd0683ef0a18a1b7d25c67cd39fa15ddad3fba
Manish Gopal Singh <manish.singh@yipl.com.np>
2016-11-29 16:18:56 +0545

    add postgreql

--- .ebextensions/scripts/setup-all.sh
+++ .ebextensions/scripts/setup-all.sh
@@ -2,3 +2,4 @@ export PATH=$PATH:/usr/local/bin:/usr/local/sbin:/usr/sbin
 bash /home/ec2-user/scripts/setup-beanstalk.sh
 bash /home/ec2-user/scripts/setup-supervisor.sh
 bash /home/ec2-user/scripts/setup-pdfprocessor.sh
+bash /home/ec2-user/scripts/setup-postgresql.sh
