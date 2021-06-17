2f5638b23068b53a9853203647a8973353ab4d57
mabuturabcloudelligent <abu.turab@cloudelligent.com>
2021-06-18 01:25:42 +0500

    logs added for debugging RC MTurk

--- app/Nrgi/Mturk/Services/MechanicalTurkV2.php
+++ app/Nrgi/Mturk/Services/MechanicalTurkV2.php
@@ -414,7 +414,7 @@ class MechanicalTurkV2
     public function updateExpirationForHIT($hit_id, $expire_at)
     {
         $this->content                         = json_encode(array('HITId' => $hit_id, 'ExpireAt' => $expire_at));
-        Log::info( 'content is'.json_encode(array('HITId' => $hit_id, 'ExpireAt' => $expire_at)));
+        $log::info( 'content is'.json_encode($this->content   ))
         $this->request_headers['x-amz-target'] = $this->metadata['targetPrefix'].".UpdateExpirationForHIT";
 
         $resp = $this->curlRequest();
@@ -432,7 +432,7 @@ class MechanicalTurkV2
     public function deleteHIT($hit_id)
     {
         $this->content                         = json_encode(array('HITId' => $hit_id));
-        Log::info( 'content inside delete is'.json_encode(array('HITId' => $hit_id)));
+        $log::info( 'content inside deleteis'.json_encode($this->content))
         $this->request_headers['x-amz-target'] = $this->metadata['targetPrefix'].".DeleteHIT";
 
         $resp = $this->curlRequest();
