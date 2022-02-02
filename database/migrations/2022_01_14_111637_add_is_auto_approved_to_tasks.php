8a2906e4e42b76d965ade2743d63caef064475ce
sriza <sriza054@gmail.com>
2022-02-02 15:17:38 +0545

    1323-mturk-resubmit
    - [x] reset auto-approved HIT
    - [x] text explainer added

---
+++ database/migrations/2022_01_14_111637_add_is_auto_approved_to_tasks.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class AddIsAutoApprovedToTasks
+ */
+class AddIsAutoApprovedToTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mturk_tasks', function (Blueprint $table) {
+            $table->boolean('is_auto_approved')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mturk_tasks', function (Blueprint $table) {
+            $table->dropColumn('is_auto_approved');
+        });
+    }
+}
