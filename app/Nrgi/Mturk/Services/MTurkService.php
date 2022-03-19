cf10039de06197ee5f8812ec31c8f6d306732bdc
mabuturab1 <m.abuturab1@gmail.com>
2022-03-20 00:36:26 +0500

    qualification requirements applied back

--- app/Nrgi/Mturk/Services/MTurkService.php
+++ app/Nrgi/Mturk/Services/MTurkService.php
@@ -85,7 +85,7 @@ class MTurkService extends MechanicalTurkV2
             'LifetimeInSeconds'           => config('mturk.defaults.production.LifetimeInSeconds'),
             'Question'                    => $this->getQuestionXML($question_url),
             'MaxAssignments'              => config('mturk.defaults.production.MaxAssignments'),
-            // 'QualificationRequirements'   => config('mturk.defaults.production.QualificationRequirements'),
+            'QualificationRequirements'   => config('mturk.defaults.production.QualificationRequirements'),
             'AutoApprovalDelayInSeconds' => config('mturk.defaults.production.AutoApprovalDelayInSeconds')
         ];
 
