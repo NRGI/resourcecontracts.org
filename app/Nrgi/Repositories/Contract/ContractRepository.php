1ae2d548f27aa236dd30c1130cb6dd7d938484be
mabuturab1 <m.abuturab1@gmail.com>
2022-10-12 12:26:22 +0500

    Filtered by created datetime

--- app/Nrgi/Repositories/Contract/ContractRepository.php
+++ app/Nrgi/Repositories/Contract/ContractRepository.php
@@ -93,10 +93,8 @@ class ContractRepository implements ContractRepositoryInterface
             if(count($publishing_year_arr) >= 2) 
             {
                 $query->whereRaw("contracts.publishing_date->'metadata'->>'status' =?", [$contractPublishStatus]);
-                $query->whereBetween(DB::raw("to_date(contracts.publishing_date->'metadata'->>'datetime', 'YYYY-MM-DD')"), [$publishing_year_arr[0], $publishing_year_arr[1]]);
+                $query->whereBetween(DB::raw("DATE(contracts.created_datetime)"), [$publishing_year_arr[0], $publishing_year_arr[1]]);
             }
-           
-
         }
         
         if (isset($type) && $type == 'ocr' && $status != '') {
