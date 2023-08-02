664c361c722ba4c1a12d9a2f9bdcbf604a1e163c
mabuturab1 <m.abuturab1@gmail.com>
2023-08-02 19:17:24 +0500

    OCR dropdown text updates

--- resources/lang/en/ocr_status.php
+++ resources/lang/en/ocr_status.php
@@ -6,6 +6,6 @@
 return [
     1      => "Acceptable",
     2      => "Needs Editing",
-    3      => "Needs full transcription",
-    'non'  => "Not available"
+    3      => "Needs Full Transcription",
+    'non'  => "Not Available"
 ];
