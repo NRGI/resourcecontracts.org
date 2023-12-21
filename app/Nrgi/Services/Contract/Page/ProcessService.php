bffe85b7ff66c2300eb0ebf2e94297cf1c176b49
mabuturab1 <m.abuturab1@gmail.com>
2023-12-21 20:05:18 +0500

    Minor fix

--- app/Nrgi/Services/Contract/Page/ProcessService.php
+++ app/Nrgi/Services/Contract/Page/ProcessService.php
@@ -386,7 +386,7 @@ class ProcessService
             'fr' => 'french',
             'es' => 'spanish',
             'en' => 'english',
-            "pt" => "portuguese",
+            "pt" => "portuguesestandard",
             "ar" => "arabic",
         ];
 
