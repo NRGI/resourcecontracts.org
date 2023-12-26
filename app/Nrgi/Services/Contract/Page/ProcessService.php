18d9a8f255991252b995d15bf49f8aac12280a8f
mabuturabcloudelligent <79276394+mabuturabcloudelligent@users.noreply.github.com>
2023-12-26 18:57:21 +0500

    Minor fix (#1587)
    Co-authored-by: mabuturab1 <m.abuturab1@gmail.com>

--- app/Nrgi/Services/Contract/Page/ProcessService.php
+++ app/Nrgi/Services/Contract/Page/ProcessService.php
@@ -387,7 +387,7 @@ class ProcessService
             'fr' => 'french',
             'es' => 'spanish',
             'en' => 'english',
-            "pt" => "portuguese",
+            "pt" => "portuguesestandard",
             "ar" => "arabic",
         ];
 
